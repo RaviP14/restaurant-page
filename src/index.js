fb826e071f0dd46d5d1f074c2f826c57e44c8e81
RaviP14 <ravi.patel14@outlook.com>
2021-06-22 17:48:05 +0100

    updated pages.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { homepage } from './home'
+import { menu } from './menu'
 
 const pageLoad = (() => {
     window.onload = homepage;
+    //window.onload = menu;
 })();
 
 console.log('Hi restaurant!');
