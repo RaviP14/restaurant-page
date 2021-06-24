46816ceb305e58d23f22e27adecd74391729fb5e
RaviP14 <ravi.patel14@outlook.com>
2021-06-24 16:58:53 +0100

    Added more text.

--- src/home.js
+++ src/home.js
@@ -7,13 +7,17 @@ const homepage = (() => {
     content.appendChild(homeDiv);
 
     let description = document.createElement('p');
-    description.textContent = 'A La Restuarant has spend decades perfecting their menu, \r\njoin us and enjoy the great atmosphere and customer service.';
-    description.setAttribute('style', 'white-space: pre;');
+    description.textContent = 'We\'ve spent decades perfecting our menu';
     homeDiv.appendChild(description);
-    
-    document.body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2021/02/06/19/29/pancakes-5989136_1280.jpg')";
+
+    let midText = document.createElement('p');
+    midText.textContent = 'Join us and enjoy the great atmosphere and customer service \r\nor oder online!'
+    midText.setAttribute('style', 'white-space: pre;');
+    homeDiv.appendChild(midText);
+
+    /*document.body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2013/12/11/13/49/holiday-226830_1280.jpg')";
     document.body.style.backgroundRepeat = 'no-repeat';
-    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundSize = 'cover'; */
 });
 
 export { homepage }
