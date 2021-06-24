e173dcd4deadc25a05ef2f9410b81c477d5426e5
RaviP14 <ravi.patel14@outlook.com>
2021-06-24 13:02:37 +0100

    Added tab switching logic to page.

--- src/index.js
+++ src/index.js
@@ -12,17 +12,26 @@ const tabs = (() => {
     let contactBtn = document.querySelector('.contactbtn');
 
     homeBtn.addEventListener('click', () => {
+        removePage()
         homepage()
     })
 
     menuBtn.addEventListener('click', () => {
+        removePage()
         menu()
     })
 
     contactBtn.addEventListener('click', () => {
+        removePage()
         contactUs()
     })
 
+    function removePage() {
+        let contents = document.getElementById('content');
+        while (contents.hasChildNodes()) {
+            contents.removeChild(contents.firstChild);
+        }
+    }
 })();
 
 console.log('Hi restaurant!');
