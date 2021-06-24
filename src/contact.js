d16855fe53770ff166552d336c1c1fcfdac94892
RaviP14 <ravi.patel14@outlook.com>
2021-06-24 20:04:06 +0100

    Added mor contact info.

--- src/contact.js
+++ src/contact.js
@@ -10,10 +10,14 @@ const contactUs = (() => {
     header.textContent = 'Contact Us';
 
     let address = document.createElement('p');
-    address.textContent = '451 Restaurant, Street, Food Heaven, F33 H45';
+    address.textContent = 'Location: 451 Restaurant Street, Food Heaven, F33 H45';
+
+    let email = document.createElement('p');
+    email.textContent = 'Email: alarestaurant@fake-mail.com';
 
     contactDiv.appendChild(header);
     contactDiv.appendChild(address);
+    contactDiv.appendChild(email);
 })
 
 export { contactUs }
